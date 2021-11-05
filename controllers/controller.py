bf7f971d999913589690df99dfdc9cb16fb1909d
rogertmalg <roger.malgueira@outlook.com>
2021-11-05 13:10:44 +0000

    adds results page and route

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,17 @@
 from flask import render_template #might need request, redirect
 from app import app
+from models.game import *
+from models.player import *
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/<player_1_hand>/<player_2_hand>')
+def result(player_1_hand, player_2_hand):
+    player_1 = Player("Player 1", player_1_hand)
+    player_2 = Player("Player 2", player_2_hand)
+    game_object = Game()
+    result = game_object.play_game(player_1, player_2)
+    
+    return render_template('result.html', player_1 = player_1, player_2 = player_2, winner = result)
--- models/game.py
+++ models/game.py
@@ -6,17 +6,17 @@ class Game():
             "rock" : "scissors"           
         }
 
-def play_game(self, player_1, player_2):
+    def play_game(self, player_1, player_2):
     
-    winner = None
+        winner = None
 
-    if self.winner_check[player_1.hand.lower()] == player_2.hand.lower():
-        winner = player_1
+        if self.winner_check[player_1.hand.lower()] == player_2.hand.lower():
+            winner = player_1
 
-    elif self.winner_check[player_2.hand.lower()] == player_1.hand.lower():
-        winner = player_2
+        elif self.winner_check[player_2.hand.lower()] == player_1.hand.lower():
+            winner = player_2
 
-    return winner
+        return winner
 
     # did check homework file for this part, logic behind this is that if 
     # player_1 chose rock, the winner_check will check the value of the 
